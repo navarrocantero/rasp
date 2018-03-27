f4764a1183e20baa60b18eba32dadfad36e914ad
Jose Antonio Navarro Cantero <navarrocantero@MacBook-Pro-de-Jose.local>
2018-03-27 19:24:16 +0200

    first commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,20 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use PiPHP\GPIO\GPIO;
+use PiPHP\GPIO\Pin\PinInterface;
 
 Route::get('/', function () {
+
+    // Create a GPIO object
+    $gpio = new GPIO();
+
+// Retrieve pin 18 and configure it as an output pin
+    $pin = $gpio->getOutputPin(18);
+
+// Set the value of the pin high (turn it on)
+    $pin->setValue(PinInterface::VALUE_HIGH);
     return view('welcome');
+
+
 });
