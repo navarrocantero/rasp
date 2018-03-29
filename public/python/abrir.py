b9663c75b5ac82e2da6195b6e12db67acbd3547f
Jose Antonio Navarro Cantero <navarrocantero@MacBook-Pro-de-Jose.local>
2018-03-29 08:28:49 +0200

    first

--- public/python/abrir.py
+++ public/python/abrir.py
@@ -2,6 +2,7 @@
 import serial
 
 arduino = serial.Serial('/dev/ttyUSB0', 9600,timeout=1)
-arduino.write('0')
+time.sleep(5)
+arduino.write('A')
 print(arduino.read())
 arduino.close()
--- public/python/cerrar.py
+++ public/python/cerrar.py
@@ -2,7 +2,8 @@
 import serial
 
 arduino = serial.Serial('/dev/ttyUSB0', 9600,timeout=1)
-arduino.write('1')
+time.sleep(5)
+arduino.write('C')
 print(arduino.read())
 
 arduino.close() #Finalizamos la comunicacion
