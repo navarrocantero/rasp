d0776cc47acbeb2b067f0ab87d3e8982b140a8dd
Jose Antonio Navarro Cantero <navarrocantero@MacBook-Pro-de-Jose.local>
2018-03-29 05:57:59 +0200

    first

--- public/python/rasp.py
+++ public/python/rasp.py
@@ -1,19 +1,12 @@
 
 import serial
 
-arduino = serial.Serial('/dev/cu.wchusbserial141140', 9600,timeout=1)
+arduino = serial.Serial('/dev/cu.ttyUSB0', 9600,timeout=1)
 
 print("Starting!")
-
-
-#      comando = raw_input('Introduce un comando: ') #Input
 comando = 'A'
-arduino.write(comando) #Mandar un comando hacia Arduino
-if comando == 'A':
- print('LED APAGADO')
-elif comando == 'E':
- print('LED ENCENDIDO')
-
+arduino.write(comando)
+print('LED APAGADO')
 
 print(arduino.read())
 
