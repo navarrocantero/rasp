f5a910009c9d7c4f2875a835879099838a54113e
Jose Antonio Navarro Cantero <navarrocantero@MacBook-Pro-de-Jose.local>
2018-03-29 05:12:58 +0200

    first

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,29 +1,13 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-use PiPHP\GPIO\GPIO;
-use PiPHP\GPIO\Pin\PinInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 Route::get('/', function () {
-
-    // Create a GPIO object
-    $gpio = new GPIO();
-
-// Retrieve pin 18 and configure it as an output pin
-    $pin = $gpio->getOutputPin(18);
-
-// Set the value of the pin high (turn it on)
-    $pin->setValue(PinInterface::VALUE_HIGH);
+    $process = new Process('python python/rasp.py');
+    $process->run();
+    if (!$process->isSuccessful()) {
+        throw new ProcessFailedException($process);
+    }
     return view('welcome');
-
 
 });
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Laravel</title>
+    @include('layout.scripts')
+    @include('layout.styles')
+</head>
+<body>
+<div class="container-fluid">
+    <div class="col-12">
+        <div class="navbar bg-danger">
+
+        </div>
+    </div>
+    <div class="col">
+
+        <div class="btn btn-danger mt-5">1</div>
+    </div>
+</div>
+</body>
+</html>
