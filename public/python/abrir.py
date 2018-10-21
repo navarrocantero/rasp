c6fd54a82ba67a591e21209a3cf5bf3bed5a04d5
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2018-10-21 12:27:09 +0200

    Pr1
    Añadido print para saber que comando se ejecuta

--- public/python/abrir.py
+++ public/python/abrir.py
@@ -7,5 +7,6 @@ time.sleep(2)
 arduino.flush()
 comando = ('A') #Input
 arduino.write(comando) #Mandar un comando hacia Arduino
+print("Comando a ejecutar... ")
 print(arduino.read())
 arduino.close() #Finalizamos la comunicacion
